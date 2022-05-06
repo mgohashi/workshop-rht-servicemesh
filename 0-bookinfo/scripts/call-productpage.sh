6101b11fdab9e08216aced01bd9e77cef509fc0a
Marcelo Ohashi <mohashi@redhat.com>
2022-05-06 13:41:23 -0300

    Updating script

--- 0-bookinfo/scripts/call-productpage.sh
+++ 0-bookinfo/scripts/call-productpage.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+SLEEP=0.1
+HOST=$(oc get route -lmaistra.io/gateway-name=bookinfo-gateway -n istio-system -ojsonpath='{.items[].spec.host}')
+
 while true; do 
-    echo call-$(date | tr -d ' ')
-    curl -sk http://istio-ingressgateway-istio-system.apps.cluster-dfkdw.dfkdw.sandbox1648.opentlc.com/productpage > /dev/null
-    sleep 0.5
+    HTTP_CODE=$(curl -vk http://$HOST/productpage 2>&1 | grep -oP "(HTTP/1.[0-1] [0-9]{3} .+)")
+    echo "$(date) - $HTTP_CODE"
+    sleep $SLEEP
 done
